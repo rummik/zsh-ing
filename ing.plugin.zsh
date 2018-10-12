e06eaf94e28be1e84c66ce7772c9607b3441b454
*Kim Zick (rummik) <k@9k1.us>
2018-10-12 13:15:33 -0400

    Remove PCRE requirement

--- ing.plugin.zsh
+++ ing.plugin.zsh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-zmodload zsh/pcre
-
 ing() {
 	local host=$1 nl
 	ping $1 | while read line; do
-		if [[ $line -pcre-match 'time=(\d+\.?\d* ms)' ]]; then
-			print -n "$nl$host: ${match[1]}"
+		if [[ $line != ${line//*time=} ]]; then
+			print -n "$nl$host: ${line//*time=}"
 			nl="\n"
 		fi
 	done
